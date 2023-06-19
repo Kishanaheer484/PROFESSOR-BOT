0be9bf42c42925097fe1ef653e41a4d1a2b8ee10
Kishanaheer484 <133010127+Kishanaheer484@users.noreply.github.com>
2023-06-19 15:08:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,6 +248,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/newSouthmovieinhindid') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
